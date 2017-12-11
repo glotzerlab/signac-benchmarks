9a418f83649fec78abbcac277525f221dfd96da4
Carl Simon Adorf <csadorf@umich.edu>
2017-12-11 17:37:56 -0500

    Ignore local id for database upload.

--- upload.py
+++ upload.py
@@ -4,4 +4,5 @@ db = signac.get_database('testing')
 
 with signac.Collection.open('benchmark.txt') as c:
     for doc in c:
+        del doc['_id']
         db.signac_benchmarks.replace_one(doc['meta'], doc, upsert=True)
