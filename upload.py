0702314b258d5dff406e1daf12563b456e70fea6
Carl Simon Adorf <csadorf@umich.edu>
2017-12-17 11:48:31 -0500

    Translate keys with dots in upload script.
    A MongoDB collection can't cope with dots in the key field.

--- upload.py
+++ upload.py
@@ -1,11 +1,25 @@
+import argparse
+
 import signac
 from signac.contrib.collection import _traverse_tree
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'filename', nargs='?', default='benchmark.txt',
+    help="The name of the benchmark collection file.")
+args = parser.parse_args()
+
+
 db = signac.get_database('testing')
 
-with signac.Collection.open('benchmark.txt') as c:
+with signac.Collection.open(args.filename) as c:
     for doc in c:
         del doc['_id']
+        try:
+            dtr_version = doc['meta']['versions'].pop('datreant.core')
+            doc['meta']['versions']['datreant_core'] = dtr_version
+        except KeyError:
+            pass
         key = dict(_traverse_tree(doc['meta'], key='meta'))
         key['profile'] = {'$ne' if doc.get('profile') else '$eq': None}
         db.signac_benchmarks.replace_one(key, doc, upsert=True)
