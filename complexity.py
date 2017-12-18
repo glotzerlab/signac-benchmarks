eec72041e8adaf577b8889ca748f95d3ecd7ae5c
Carl Simon Adorf <csadorf@umich.edu>
2017-12-18 18:24:53 -0500

    Expected time complexity for 'determine_len' is O(N).
    Although the determination of the directories (ls/listdir) is expected
    to have complexity O(log(N)), the function is dominated by checking
    whether those directories should be considered part of the data space.

--- complexity.py
+++ complexity.py
@@ -3,6 +3,6 @@ COMPLEXITY = {
     'iterate_single_pass': 'N',
     'search_lean_filter': 'N',
     'search_rich_filter': 'N',
-    'determine_len': 'log(N)',
+    'determine_len': 'N',
     'select_by_id': '1',
 }
